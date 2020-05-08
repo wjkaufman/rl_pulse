cfb3ab64fa7367e680b89bc242f79e0c5aac5533
wjkaufman <will@wjkaufman.com>
2020-05-08 09:25:50 -0600

    Change hyperparameters, add walltime

--- code/makeJobs.py
+++ code/makeJobs.py
@@ -8,27 +8,30 @@
 import shutil
 import sys
 
-lstmLayers = [1, 2]
-lstmUnits = [16,64]
-actorLRs = [1e-3]
-updateEverys = [50]
+lstmLayers = [1,2]
+denseLayers = [4,8]
+# units = [64] # for both LSTM and dense layers
+gamma = [0.5, 0.9]
+batchSizes = [250, 1000]
+updateEverys = [20, 40]
 
 i = 0
 
 for a in lstmLayers:
-    for b in lstmUnits:
-        for c in actorLRs:
-            for d in updateEverys:
-                # copy job template
-                shutil.copyfile(sys.argv[1], f"job{i:03}.pbs")
-                jobFile = open(f"job{i:03}.pbs", 'a')
-                # create function call
-                call = f"python -u runRLPulse.py {100000} {a} {4} {b} {b} {c} {c*10} {.01} {.9} {10000} {250} {1000} {d}"
-                print(call)
-                jobFile.write("echo " + call + "\n")
-                jobFile.write(call)
-                jobFile.write("\n\nexit 0\n")
-                jobFile.close()
-                i += 1
+    for b in denseLayers:
+        for c in batchSizes:
+            for d in gamma:
+                for e in updateEverys:
+                    # copy job template
+                    shutil.copyfile(sys.argv[1], f"job{i:03}.pbs")
+                    jobFile = open(f"job{i:03}.pbs", 'a')
+                    # create function call
+                    call = f"python -u runRLPulse.py {100000} {a} {b} {64} {64} {1e-4} {1e-3} {.01} {d} {10000} {c} {1000} {e}"
+                    print(call)
+                    jobFile.write("echo " + call + "\n")
+                    jobFile.write(call)
+                    jobFile.write("\n\nexit 0\n")
+                    jobFile.close()
+                    i += 1
 
 print("Done!")
