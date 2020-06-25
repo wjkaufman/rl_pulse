18dd2a4b6765921cdccb34e7100b6dbae4c59689
wjkaufman <will@wjkaufman.com>
2020-06-24 18:46:20 -0600

    Update job plot script

--- code/plots.R
+++ code/plots.R
@@ -2,42 +2,46 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 
+setwd('~/projects/rl_pulse/data/2020-06-23/')
+
 # create plots for each directory that matches regex pattern
 for (datFolder in list.files(pattern = "00*")) {
   # navigate to directory
   setwd(datFolder)
   print(paste("In directory:", datFolder))
+  suffix = substr(datFolder, 0, 3)
   # make plots
   paramDiff = read.csv("paramDiff.csv")
+  paramDiff$layerNum = factor(paramDiff$layerNum, ordered = T)
   for (networkType in c("actor", "critic")) {
     g = ggplot(paramDiff %>% filter(type==networkType),
                aes(x=generation, y=diff, group=layerNum, color=layerNum)) +
-      geom_line(size=1) + 
+      geom_line() + 
       labs(title=paste(networkType, "parameter difference"),
            x='Generation',
            y='Parameter difference',
            color="Layer Number")+
-      # scale_y_log10() + 
+      scale_y_log10()
       theme_bw()
     # print(g)
-    ggsave(paste0(networkType, "_paramDiff.png"), scale=1.5, width=5, height=3, units="in")
+    ggsave(paste0("../graphics/", networkType, "_paramDiff-", suffix, ".png"),
+           scale=1.5, width=5, height=3, units="in")
   }
   
   popFitnesses = read.csv("popFitnesses.csv")
   popFitnesses$individual = as.factor(popFitnesses$individual)
   g = ggplot(popFitnesses,
-             aes(x=generation, y=fitness, group=individual, shape=individual, size=fitnessInd,
+             aes(x=generation, y=fitness, group=individual, size=fitnessInd,
                  color=synced)) +
     geom_point(alpha=0.8) + 
     labs(title="Population fitnesses",
          x='Generation',
          y='Fitness',
          size="Max reward index",
-         color="Individual")+
-    # scale_y_log10() + 
+         color="Synced")+
     theme_bw()
   # print(g)
-  ggsave("popFitnesses.png", scale=1.5, width=5, height=3, units="in")
+  ggsave(paste0("../graphics/popFitnesses-", suffix, ".png"), scale=1.5, width=5, height=3, units="in")
   
   testFitnesses = read.csv("testMat.csv")
   g = ggplot(testFitnesses,
@@ -46,11 +50,12 @@ for (datFolder in list.files(pattern = "00*")) {
     labs(title="Test fitnesses",
          x='Generation',
          y='Fitness',
+         color='Type',
          size="Max reward index")+
     # scale_y_log10() + 
     theme_bw()
   # print(g)
-  ggsave("testFitnesses.png", scale=1.5, width=5, height=3, units="in")
+  ggsave(paste0("../graphics/testFitnesses-", suffix, ".png"), scale=1.5, width=5, height=3, units="in")
   
   # pack out to parent directory
   setwd("..")
