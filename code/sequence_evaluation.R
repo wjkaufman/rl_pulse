69611e7bae80f415d445a57caf958dab7bba2eac
wjkaufman <will@wjkaufman.com>
2020-07-06 13:06:56 -0600

    Change sequence_evaluation R script to match updated nb

--- code/sequence_evaluation.R
+++ code/sequence_evaluation.R
@@ -1,22 +1,25 @@
 library(ggplot2)
+library(dplyr)
 
 setwd("~/projects/rl_pulse/data/2020-07-01-eval/")
 
+df.whh = read.csv("WHH.csv")
 df = read.csv("candidate4.csv")
+df = bind_rows(df, df.whh)
 #df$type[df$type=="candidate"] = "Candidate"
-df$reward = -log10(1-df$fidelity + 1e-100)
-# df$time = ifelse(df$type=="Candidate", 3*df$delay + 12*df$pulseWidth, 6*df$delay + 4*df$pulseWidth)
-df[,c("delay", "pulseWidth")] = signif(df[,c("delay", "pulseWidth")], digits = 2)
+#df$reward = -log10(1-df$fidelity + 1e-100)
+# df$time = ifelse(df$type=="Candidate", 3*df$delay + 12*df$pulse_width, 6*df$delay + 4*df$pulse_width)
+df[,c("delay", "pulse_width")] = signif(df[,c("delay", "pulse_width")], digits = 2)
 
 g = ggplot(df, aes(x=reward, group=type, fill=type)) +
   geom_histogram(bins=50, alpha = 0.6, position = 'identity') +
-  facet_grid(rows=vars(delay), cols=vars(pulseWidth), labeller = label_both) +
+  facet_grid(rows=vars(delay), cols=vars(pulse_width), labeller = label_both) +
   theme_minimal() +
   labs(x="Reward", y="Count", fill="Sequence")
 print(g)
 ggsave("reward_hist.pdf", width = 8, height=6)
 
-g = ggplot(df, aes(x=delay, y=time, size=pulseWidth, group=type, color=type)) +
+g = ggplot(df, aes(x=delay, y=time, size=pulse_width, group=type, color=type)) +
   geom_point()
 print(g)
 
