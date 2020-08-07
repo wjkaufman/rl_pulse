7592b90aeef793eba0e559e7fb108f606f29d6d4
wjkaufman <will@wjkaufman.com>
2020-08-07 12:27:21 -0600

    Add tests

--- rl_pulse/spin_simulation_test.py
+++ rl_pulse/spin_simulation_test.py
@@ -1,5 +1,43 @@
-import rl_pulse as rlp
-import numpy as np
 import unittest
+import spin_simulation as ss
+import numpy as np
 
-# TODO fill in unit tests here...
+
+class SpinSimulationTest(unittest.TestCase):
+    
+    def setUp(self):
+        '''Set up a 4 spin-1/2 system.
+        '''
+        self.N = 4
+        self.dim = 2**4
+    
+    def test_mykron(self):
+        self.assertTrue(np.array_equal(
+            ss.mykron(ss.z, ss.z),
+            np.array([[1, 0, 0, 0],
+                      [0, -1, 0, 0],
+                      [0, 0, -1, 0],
+                      [0, 0, 0, 1]]) / 4))
+        self.assertTrue(np.array_equal(
+            ss.mykron(ss.x, ss.x),
+            np.array([[0, 0, 0, 1],
+                      [0, 0, 1, 0],
+                      [0, 1, 0, 0],
+                      [1, 0, 0, 0]]) / 4))
+        self.assertTrue(np.array_equal(
+            ss.mykron(ss.z, ss.z, ss.z),
+            np.array([[1, 0, 0, 0, 0, 0, 0, 0],
+                      [0, -1, 0, 0, 0, 0, 0, 0],
+                      [0, 0, -1, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 1, 0, 0, 0, 0],
+                      [0, 0, 0, 0, -1, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 1, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 1, 0],
+                      [0, 0, 0, 0, 0, 0, 0, -1]]) / 8))
+    
+    # def test():
+    #     pass
+
+
+if __name__ == '__main__':
+    unittest.main()
