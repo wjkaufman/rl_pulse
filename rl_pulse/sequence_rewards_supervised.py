Correct pulse_width size, fix action unitary bug, lower precision for r

rl_pulse/sequence_rewards_supervised.py
@@ -23,11 +23,11 @@ def generate_data(
         delta=500,
         coupling=1e3,
         delay=10e-6,
-        pulse_width=500,
+        pulse_width=1e-6,
         # data parameters
-        num_configurations=1000,
-        log_configuration=20,
-        num_sequences_per_configuration=1000,
+        num_configurations=100,
+        log_configuration=10,
+        num_sequences_per_configuration=100,
         sequence_length=24,  # multiple of 6
         output_path='.',
         filename='data.npz'
@@ -48,15 +48,16 @@ def generate_data(
         Utarget_step = ss.get_propagator(H_target, (pulse_width + delay))
         
         # define actions
-        Udelay = linalg.expm(-1j*(Hint*(pulse_width + delay)))
-        Ux = linalg.expm(-1j*(X*np.pi/2 + Hint*pulse_width))
-        Uxbar = linalg.expm(-1j*(X*-np.pi/2 + Hint*pulse_width))
-        Uy = linalg.expm(-1j*(Y*np.pi/2 + Hint*pulse_width))
-        Uybar = linalg.expm(-1j*(Y*-np.pi/2 + Hint*pulse_width))
+        Udelay = linalg.expm(-1j * Hint * delay)
+        Ux = linalg.expm(-1j * (X*np.pi/2 + Hint*pulse_width))
+        Uxbar = linalg.expm(-1j * (X*-np.pi/2 + Hint*pulse_width))
+        Uy = linalg.expm(-1j * (Y*np.pi/2 + Hint*pulse_width))
+        Uybar = linalg.expm(-1j * (Y*-np.pi/2 + Hint*pulse_width))
         Ux = Udelay @ Ux
         Uxbar = Udelay @ Uxbar
         Uy = Udelay @ Uy
         Uybar = Udelay @ Uybar
+        Udelay = linalg.expm(-1j*(Hint*(pulse_width + delay)))
         action_unitaries = [Ux, Uxbar, Uy, Uybar, Udelay]
         
         for s in range(num_sequences_per_configuration):
@@ -69,7 +70,7 @@ def generate_data(
             for t in range(sequence_length):
                 action_ind = np.random.choice(5)
                 action = np.array(np.arange(5) == action_ind,
-                                  dtype=np.float32)
+                                  dtype=np.int8)
                 sequence.append(action)
                 Utarget = Utarget_step @ Utarget
                 Uexp = action_unitaries[action_ind] @ Uexp
@@ -78,7 +79,7 @@ def generate_data(
                 sequence_rewards.append(r)
             
             actions.append(np.stack(sequence))
-            rewards.append(np.stack(sequence_rewards))
+            rewards.append(np.stack(sequence_rewards).astype(np.float16))
         
     actions = np.stack(actions)
     rewards = np.stack(rewards)
@@ -117,7 +118,9 @@ def train_eval(
         rewards_train,
         actions_val,
         rewards_val,
-        output_path
+        output_path,
+        batch_size=64,
+        epochs=50,
         ):
     
     model = keras.Sequential(
@@ -136,8 +139,8 @@ def train_eval(
     model.fit(
         actions_train,
         rewards_train,
-        batch_size=64,
-        epochs=10,
+        batch_size=batch_size,
+        epochs=epochs,
         validation_data=(actions_val, rewards_val),
         callbacks=[keras.callbacks.TensorBoard(
             log_dir=os.path.join(output_path, 'logs')